a61a5a5395646655dbafc287825cd28c5ddf472a
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-11-24 00:50:27 +0100

    imported ticket form

--- resources/js/Pages/Tickets/Create.jsx
+++ resources/js/Pages/Tickets/Create.jsx
@@ -1,8 +1,27 @@
-const Create = () => {
+import TicketForm from "./components/TicketForm";
+
+const Create = ({ categories, users }) => {
+
+    const defaultValues = {
+        title: '',
+        description: '',
+        operator: '',
+        category: ''
+    }
+
     return (
         <section>
             <div className="container mx-auto">
-                <h1 className="my-12 text-4xl text-center">Crea nuovo ticket</h1>
+                <h1 className="mt-12 text-4xl text-center">Crea nuovo ticket</h1>
+
+                <TicketForm
+                    users={users}
+                    categories={categories}
+                    action={route('tickets.store')}
+                    method='post'
+                    defaultValues={defaultValues}
+                />
+
             </div>
         </section>
     )
