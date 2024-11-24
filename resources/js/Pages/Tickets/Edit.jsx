c4446ea6fb8901f4e1459c971cb58bcc1cc2c33c
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-11-24 01:48:08 +0100

    imported ticket form

--- resources/js/Pages/Tickets/Edit.jsx
+++ resources/js/Pages/Tickets/Edit.jsx
@@ -1,8 +1,25 @@
-const Edit = ({ ticket }) => {
+import TicketForm from "./components/TicketForm";
+
+const Edit = ({ ticket, users, categories }) => {
+
+    const defaultValues = {
+        title: ticket.title,
+        description: ticket.description,
+        user: ticket.user_id,
+        category: ticket.category_id
+    }
+
     return (
         <section>
             <div className="container mx-auto">
-                <h1 className="my-12 text-center text-4xl">Edit {ticket.title}</h1>
+                <h1 className="my-12 text-center text-4xl">Modifica {ticket.title}</h1>
+                <TicketForm
+                    users={users}
+                    categories={categories}
+                    action={route('tickets.update', ticket.slug)}
+                    method='put'
+                    defaultValues={defaultValues}
+                />
             </div>
         </section>
     )
